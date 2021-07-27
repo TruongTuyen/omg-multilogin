149477ca1688eae1fea93c3b83c8bd1fb8e88405
Thang Le <thangbiet17@gmail.com>
2021-07-27 17:15:37 +0700

    Test commit.

--- app/src/views/team-members/teamMembers.tsx
+++ app/src/views/team-members/teamMembers.tsx
@@ -3,7 +3,6 @@ import { PageHeader } from 'antd';
 
 export default class TeamMembers extends React.Component {
     render(): JSX.Element {
-        console.log('team-members');
         return (
             <div>
                 <PageHeader title='Team members' />
